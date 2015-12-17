836652adfe3590d97dabf38025b364501af263a9
Jacob Floyd <cognifloyd@gmail.com>
2015-12-16 19:48:54 -0500

    [TASK] Refactor visual node, Visual directive into rst package

---
+++ visuals/rst/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    visuals.rst
+    ~~~~~~~~~~~
+
+    This package is a namespace package that contains ``visuals.rst``
+
+    :copyright: Copyright 2015 by the contributors, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
---
+++ visuals/rst/nodes.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+    visuals.rst.nodes
+    ~~~~~~~~~~~~~~~~~
+
+    ReStructuredText document tree nodes for Docutils
+
+    :copyright: Copyright 2015 by the contributors, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
+from __future__ import absolute_import
+
+from docutils import nodes
+
+
+class visual(nodes.General, nodes.Element):
+    """ A Visual Node """
+    pass
